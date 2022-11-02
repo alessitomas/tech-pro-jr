599a50b2d28392e92fcff7bdaacc5a87f7543bab
Eduardosmb <edubarros253@gmail.com>
2022-11-02 19:20:48 -0300

    searchbar

--- pages/area/admin/candidato/index.js
+++ pages/area/admin/candidato/index.js
@@ -1,5 +1,6 @@
 import styles from '../../../../styles/area_admin.module.css'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users')
@@ -13,11 +14,20 @@ export const getStaticProps = async () => {
 }
 
 export default function Area_Candidato({ candidatos }) {
-  console.log(candidatos)
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div>
+      <input type="text" placeholder="Procure um candidato" onChange={event => {setSearchTerm(event.target.value);}}/>
       <h1>Candidatos</h1>
-      {candidatos.map(candidato => (
+      {candidatos.filter((val) => {
+        if (searchTerm == "") {
+          return val
+        } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return val
+        }
+
+      }).map(candidato => (
         <Link href={'candidato/'+candidato.id} key={candidato.id}>
           <div className={styles.single}>
             <h3>{candidato.name}</h3>
@@ -25,5 +35,5 @@ export default function Area_Candidato({ candidatos }) {
         </Link>
       ))}
     </div>
-  )
+      )
 }
